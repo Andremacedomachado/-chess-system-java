cc47cf1d7a4aa010c710358a8dde35727805aa6a
Andre Macedo Machado <andremacedomachado@gmail.com>
2018-09-26 15:23:06 -0300

    Placing pieces on the board

--- src/boargame/Board.java
+++ src/boargame/Board.java
@@ -1,5 +1,7 @@
 package boargame;
 
+import chess.ChessPiece;
+
 public class Board {
 	private int rows;
 	private int columns;
@@ -29,5 +31,8 @@ public class Board {
 		return pieces[position.getRow()][position.getColumn()];
 	}
 	
-	
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()]= piece;
+		piece.position = position;
+	}
 }
